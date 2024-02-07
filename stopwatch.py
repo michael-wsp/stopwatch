570325123464b40f07a73c77e39ae2ae380e4687
Michael Setchko Palmerlee <michaelsp@berkeley.edu>
2024-02-07 02:56:25 -0800

    test

--- stopwatch.py
+++ stopwatch.py
@@ -69,6 +69,7 @@ def start():
         user_input = input('Press enter to continue or type q to quit\n')
     times.close()
 
+# testing
 
 def genScramble(n):
     moves = ['R', 'M', 'U', 'L', 'D']
